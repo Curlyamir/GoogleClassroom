1e7033339aeab84eeeba75eca49ecd1f394d8b9c
Amirkh <amirh.khosh@gmail.com>
2019-06-26 14:58:13 +0430

    commit 4 //last

--- app/src/main/java/com/example/googleclassroom/MainActivity.java
+++ app/src/main/java/com/example/googleclassroom/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.googleclassroom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button register = findViewById(R.id.buttonLog);
+        Button sign = findViewById(R.id.buttonSign);
     }
 }
